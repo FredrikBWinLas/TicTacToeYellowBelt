90779b9390c5eea5db867e6eab20d4f666098d1e
Fredrik Bäckman <fredrik.backman@winlas.se>
2024-12-17 08:04:56 +0100

    player can make move

--- Test/PlayerTests.cs
+++ Test/PlayerTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using NSubstitute;
 using TicTacToeYellowBelt;
 
 namespace Test;
@@ -8,8 +9,20 @@ public class PlayerTests
     [Fact]
     public void Player_WhenChangeSymbol_ShouldChangeTheSymbol()
     {
-        var player = new Player();
+        IPlayer player = new Player();
         player.Symbol = 'X';
         player.Symbol.Should().Be('X');
+    }
+
+    [Fact]
+    public void Player_WhenMakeMove_ShouldGiveNumberFrom0To8()
+    {
+        var random = Substitute.For<Random>();
+        random.Next(0, 9).Returns(5);
+        IPlayer player = new Player();
+
+        var result = player.MakeMove();
+        result.Should().BeGreaterOrEqualTo(0);
+        result.Should().BeLessThanOrEqualTo(8);
     }
 }
--- TicTacToeYellowBelt/Player.cs
+++ TicTacToeYellowBelt/Player.cs
@@ -2,10 +2,18 @@ namespace TicTacToeYellowBelt;
 
 public class Player : IPlayer
 {
+    private Random _random = new Random();
+
+    public Player(Random? random = null)
+    {
+        _random = random ?? new Random();
+    }
     public char Symbol { get; set; }
+    public int MakeMove() => _random.Next(0, 9);
 }
 
 public interface IPlayer
 {
     char Symbol { get; set; }
+    int MakeMove();
 }

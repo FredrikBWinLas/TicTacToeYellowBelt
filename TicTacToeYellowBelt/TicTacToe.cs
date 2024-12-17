fa8d3ebe971c9f787de4c2102f8b70d950ef4c94
Fredrik Bäckman <fredrik.backman@winlas.se>
2024-12-17 08:59:25 +0100

    Player O can win in diagonal line from right to left

--- TicTacToeYellowBelt/TicTacToe.cs
+++ TicTacToeYellowBelt/TicTacToe.cs
@@ -44,6 +44,7 @@ public class TicTacToe
         if (_board[6] == _board[7] && _board[6] == _board[8] && _board[6] == symbol) return true;
 
         if (_board[0] == _board[4] && _board[4] == _board[8] && _board[0] == symbol) return true;
+        if (_board[2] == _board[4] && _board[4] == _board[6] && _board[2] == symbol) return true;
 
         return false;
     }
